e292718eaaf3c4725fa525a2a8fefc83d31ba87d
KyleGreen <KyleGreen@KGREEN-SURFACBK>
2019-10-12 11:46:46 -0700

    added input file to deployment script

--- NewRoleCreation.ps1
+++ NewRoleCreation.ps1
@@ -1,4 +1,4 @@
 
 #New Custom Azure role definition
 
-New-AzRoleDefinition -InputFile "$pathtoJSON"
+New-AzRoleDefinition -InputFile "https://kgreen0633@dev.azure.com/kgreen0633/SecurityOperations/_git/SecurityOperations/RBAC_AppDev.JSON"
