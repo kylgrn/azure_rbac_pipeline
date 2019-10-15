63b2bff2af202b20629cf347981e4a74c467e255
KyleGreen <KyleGreen@KGREEN-SURFACBK>
2019-10-14 20:19:06 -0700

    corrected file path in powershell

--- NewRoleCreation.ps1
+++ NewRoleCreation.ps1
@@ -1,4 +1,4 @@
 
 #New Custom Azure role definition
 
-New-AzRoleDefinition -InputFile "https://kgreen0633@dev.azure.com/kgreen0633/SecurityOperations/_git/SecurityOperations/RBAC_AppDev.json"
+New-AzRoleDefinition -InputFile "RBAC_AppDev.json"
