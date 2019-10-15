6f00adecd40c933bb2a67e47404c590916cfee23
KyleGreen <KyleGreen@KGREEN-SURFACBK>
2019-10-14 20:50:47 -0700

    latest

--- NewRoleCreation.ps1
+++ NewRoleCreation.ps1
@@ -1,4 +1,4 @@
 
 #New Custom Azure role definition
 
-New-AzRoleDefinition -InputFile "SecurityOperations/RBAC_AppDev.json"
+New-AzRoleDefinition -InputFile "git@ssh.dev.azure.com:v3/kgreen0633/SecurityOperations/SecurityOperations/RBAC_AppDev.json"
