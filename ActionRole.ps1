cb2fc186460b986ac522435821bd5e0b4d2d8693
KyleGreen <KyleGreen@KGREEN-SURFACBK>
2019-10-15 18:52:50 -0700

    updated ActionRole

--- ActionRole.ps1
+++ ActionRole.ps1
@@ -1,55 +1,36 @@
 
-#New Custom Azure role definition
-$RoleName = 'Custom Application Developer Admin'
-$RoleDef = Get-AzRoleDefinition $RoleName 
+$RoleTemplates = Get-ChildItem "./" -Filter *.JSON
 
-<#
-#####
-###########This is the section where you define the permissions this custom role is granted###########
-#####
-#>
-$perms = 'Microsoft.Storage/*/read','Microsoft.Network/*/read','Microsoft.Compute/*/read'
-$perms += 'Microsoft.Compute/virtualMachines/start/action','Microsoft.Compute/virtualMachines/restart/action'
-$perms += 'Microsoft.Authorization/*/read'
-$perms += 'Microsoft.ResourceHealth/availabilityStatuses/read'
-$perms += 'Microsoft.Resources/subscriptions/resourceGroups/read'
-$perms += 'Microsoft.Insights/alertRules/*','Microsoft.Support/*'
-$perms += 'Microsoft.Web/*/read'
-$perms += 'Microsoft.Web/*/write'
-<#
-#####
-######################################################################################################
-#####
-#>
 
-#These are the actions explicitly not allowed by the custom role
-$notperms = '*/delete'
-$subs = '/subscriptions/8ee2aaf4-8329-4aa2-afa2-21cda345f7d4','/subscriptions/a70ba5e6-f570-4c74-ba83-aa1f6c9f010a'
+foreach ($r in $RoleTemplates) {
 
-if ($RoleDef -eq $null)
+#Check if role exists
+$RoleGroup = Get-AzRoleDefinition -Name (get-content ('./'+$r.name) | convertfrom-json).name 
+
+
+if ($rolegroup -eq $null) 
 {
-Write-host "Existing role not found"
-$role = [Microsoft.Azure.Commands.Resources.Models.Authorization.PSRoleDefinition]::new()
-$role.Name = $RoleName
-$role.Description = 'Custom App Dev Role'
-$role.IsCustom = $true
-$role.Actions = $perms
-$role.AssignableScopes = $subs
-$role.NotActions = $notperms
-New-AzRoleDefinition -Role $role
-Write-host "Creating new Azure Role Definition"
+New-AzRoleDefinition -InputFile ('./'+$r.name) 
+
 }
-else 
+else
 {
-    write-host "Existing role found, updating permissions"
-    $ExistingRole = Get-AzRoleDefinition $RoleName
-    $Setrole = [Microsoft.Azure.Commands.Resources.Models.Authorization.PSRoleDefinition]::new()
-    $Setrole.Name = $ExistingRole.Name
-    $Setrole.id += $ExistingRole.id
-    $Setrole.Description = $ExistingRole.Description
-    $Setrole.IsCustom = $ExistingRole.IsCustom
-    $Setrole.Actions = $perms
-    $Setrole.NotActions = $notperms
-    $Setrole.AssignableScopes = $subs
- Set-AzRoleDefinition -Role $Setrole -Verbose
+$newRoleGroup = $(get-content ('./'+$r.name) | convertfrom-json)
+
+$Compare = Compare-Object $RoleGroup $newRoleGroup -Property Actions,NotActions,AssignableScopes
+
+    if( $compare -eq $null)
+        {
+            write-host 'No changes to custom definition:' $RoleGroup.name -ForegroundColor Yellow
+        }
+    else
+    {
+
+    Set-AzRoleDefinition -InputFile ('./'+$r.name)
+    write-host 'Updating permissions for custom definition: $RoleGroup.name' -ForegroundColor Green
+    }
 }
+}
+
+
+
