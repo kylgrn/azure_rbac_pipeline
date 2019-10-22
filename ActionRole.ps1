a23aa96453c2d84d0468824664959e18f412d17e
KyleGreen <KyleGreen@KGREEN-SURFACBK>
2019-10-22 10:05:36 -0700

    updated ps1

--- ActionRole.ps1
+++ ActionRole.ps1
@@ -1,5 +1,18 @@
 
+<#
+Script by Kyle Green
+kylgrn@gmail.com
 
+Purpose:
+This script is called as part of an Azure DevOps pipeline build. The script relies on JSON templates structured for custom Azure role definitions, used for custom RBAC groups. 
+When a JSON file is uploaded to the repository or an existing one is updated, the build will run and perform the following:
+1) Check to see if the role already exists in Azure
+2) If not, create new role
+3) If one exists, check to see if any of the permissions (allowed or denied) have been updated
+4) Update existing role
+#>
+
+#Inventory JSON files
 $RoleTemplates = Get-ChildItem -Path D:\a\1\s\*.JSON -Exclude package.json
 
 foreach ($r in $RoleTemplates) {
@@ -7,12 +20,13 @@ foreach ($r in $RoleTemplates) {
 #Check if role exists
 $RoleGroup = Get-AzRoleDefinition -Name (get-content ('D:\a\1\s\'+$r.name) | convertfrom-json).name 
 
-
+#If no role exists, create new one
 if ($rolegroup -eq $null) 
 {
 New-AzRoleDefinition -InputFile ('D:\a\1\s\'+$r.name) 
 
 }
+#If role already exists, check to see if permissions changed
 else
 {
 $newRoleGroup = $(get-content ('D:\a\1\s\'+$r.name) | convertfrom-json)
